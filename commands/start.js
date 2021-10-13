c15cd06958e8c4854f004aa6bf4ec68855972a87
MrMissx <hi@mrmiss.me>
2021-10-13 13:08:19 +0700

    start: Fix some string typos and unused condition check

--- commands/start.js
+++ commands/start.js
@@ -8,12 +8,10 @@ module.exports = {
 		let args = msg.content.split(" ")
 		args.shift()
 		let time = (args[0] === undefined) ? 100 : parseInt(args[0])
-		if(time <= 0 || time >= Number.MAX_SAFE_INTEGER){
-			return global.clientChannel.send(
-				`Invalid time value`
-			)
-		}
 		let ms = time * 60000
+		if(ms <= 0 || ms >= Number.MAX_SAFE_INTEGER){
+			return global.clientChannel.send("Invalid time value")
+		}
 
 		if (msg.guild.members.cache.get(target).voice.channelId === null) {
 			console.log(`>>> Failed to start session: ${msg.author.id} tried to start a session, but they're not in a voice chat channel!`);
@@ -25,23 +23,21 @@ module.exports = {
 			if (global.sessions[i] === undefined) {
 				global.sessions[i] = new Structures.Session(target, msg.guild.members.cache.get(target).voice.channelId);
 				console.log(`>>> ${msg.author.id} started a session in ${msg.guild.members.cache.get(target).voice.channelId}!`);
-				global.clientChannel.send(`<@${msg.author.id}> started in <#${msg.guild.members.cache.get(target).voice.channelId}>!`);
+				global.clientChannel.send(`<@${msg.author.id}> started a session in <#${msg.guild.members.cache.get(target).voice.channelId}>!`);
 
 				const members = global.clientGuild.channels.cache.get(global.sessions[i].channel).members;
 				const startTime = new Date()
+				let tID = setTimeout(() => {
+					msg.content = `.stop`  // Modify content for stop method parsing
+					stop.exec(msg);
+				}, ms);
+				global.sessions[i].timeoutID = tID
+
 				members.forEach((member) => {
 					global.sessions[i].log("join", member.id, startTime);
 				});
 
-				if( ms > 0){
-					let tID = setTimeout(() => {
-						msg.content = `.stop`  // Modify content for stop method parsing
-						stop.exec(msg);
-					}, ms);
-					global.sessions[i].timeoutID = tID
-					global.clientChannel.send(`Session will stop automaticaly after ${time} minute(s)`)
-				}
-
+				global.clientChannel.send(`Session will stop automatically after ${time} minute(s)`)
 				break;
 			} else {
 				if (global.sessions[i].owner === target) {
