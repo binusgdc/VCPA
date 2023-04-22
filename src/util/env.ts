edd6c7e92f9b43bf672805f73a12017701bf8326
San <istamarahsan@gmail.com>
2023-04-22 09:41:59 +0700

    env parses in Strict

--- src/util/env.ts
+++ src/util/env.ts
@@ -9,10 +9,9 @@ export type Env = {
 }
 
 export function loadEnv(): Env | undefined {
-	const env: Env = {
+	const result = schema.safeParse({
 		BOT_TOKEN: process.env.BOT_TOKEN
-	};
-	const result = schema.safeParse(env);
+	});
 	if (result.success && result.data.BOT_TOKEN != null) {
 		return {
 			BOT_TOKEN: result.data.BOT_TOKEN
