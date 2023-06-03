1d0c543c6232fdc15d90ffd5445a6f978af731cb
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2023-06-03 10:41:11 +0700

    Removed unused code + applied formatting

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,20 @@
-import { ChatInputCommandInteraction, CacheType } from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
 import CommandHandler from "./commandsHandlers/commandHandler";
 
 export default class RoutingCommandHandler implements CommandHandler {
+	private handlers: Map<string, CommandHandler>
 
-    private handlers: Map<string, CommandHandler>
-    private noRouteHandler: CommandHandler
+	public constructor(commands: { route: string, handler: CommandHandler }[]) {
+		this.handlers = new Map();
 
-    constructor(commands: { route: string, handler: CommandHandler }[], noRouteHandler?: CommandHandler) {
-        this.handlers = new Map();
-        for (const { route, handler } of commands) {
-            this.handlers.set(route, handler);
-        }
-        this.noRouteHandler = noRouteHandler ?? {
-            async handle(interaction: ChatInputCommandInteraction) {
-                await interaction.reply("Unknown command!");
-            }
-        };
-    }
-    async handle(command: ChatInputCommandInteraction): Promise<void> {
-        const target = this.handlers.get(command.commandName);
-        if (target === undefined) {
-            await this.noRouteHandler.handle(command);
-            return;
-        }
-        await target.handle(command);
-    }
+		for (const { route, handler } of commands) {
+			this.handlers.set(route, handler);
+		}
+	}
+
+	public async handle(command: ChatInputCommandInteraction): Promise<void> {
+		const target = this.handlers.get(command.commandName);
+		if (target === undefined) return;
+		await target.handle(command);
+	}
 }
